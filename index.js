2e72b8ec3e70ca87781a17409237223d7fdef210
yassimin Momed <yassiminm43@gmail.com>
2024-08-14 12:13:09 -0800

    actualização da conexao a db

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
-//configuracao inical
-const express= require('express')
-const app=express()
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
 
-//ler o json atraves do express
-app.use(
-    express.urlencoded({
-        extended:true
-    }
-    )
-)
+// Configurar o Express para usar URL encoding e JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.use(express.json())
-
-//endpoint/rota inical
-
-app.get('/',(req,res)=>{
-
-    //mostrar req
-
-    res.json({message:'mashallaaaaaah'})
+// Rota inicial
+app.get('/', (req, res) => {
+    res.json({ message: 'first api' });
 });
 
+// Configurações do MongoDB
+const DB_USER = 'meu nome';
+const DB_PASS = encodeURIComponent('@minha senha');
+const DB_URI = `mongodb+srv://${DB_USER}:${DB_PASS}@servidor1.cmcqf.mongodb.net/?retryWrites=true&w=majority&appName=Servidor1`;
 
-//entregar a porta
-app.listen(3000)
+// Conectar ao MongoDB e iniciar o servidor Express
+mongoose.connect(DB_URI)
+    .then(() => {
+        console.log("Primeira conexão a DB efetuada");
+        app.listen(3000, () => {
+            console.log('Servidor rodando na porta 3000');
+        });
+    })
+    .catch(err => console.log('Erro ao conectar ao DB:', err));

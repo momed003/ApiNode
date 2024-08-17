868720f0fc0f13c2820c451a94ec64de16d353dd
yassimin Momed <yassiminm43@gmail.com>
2024-08-17 11:41:59 -0800

    metodo post

--- index.js
+++ index.js
@@ -2,9 +2,34 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 
+//instancia da classe person
+const Person=require('./model/person')
+
 // Configurar o Express para usar URL encoding e JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+//rotas da API
+app.post('/Person',async(req,res)=>{
+
+    //req.body
+    const{name,salary,aproved}=req.body
+
+    //criar objecto
+    const Person={
+        name,
+        slary,
+        aproved
+    }
+
+    //metodo create do mongoose
+    try{
+        await Person.create(Person)
+        res.status(201).json({message:'cadastro de pessoa efectuado com sucesso'})
+    }catch(error){
+        res.status(500).json({error:error})
+    }
+})
 
 // Rota inicial
 app.get('/', (req, res) => {
@@ -12,8 +37,6 @@ app.get('/', (req, res) => {
 });
 
 // Configurações do MongoDB
-const DB_USER = 'meu nome';
-const DB_PASS = encodeURIComponent('@minha senha');
 const DB_URI = `mongodb+srv://${DB_USER}:${DB_PASS}@servidor1.cmcqf.mongodb.net/?retryWrites=true&w=majority&appName=Servidor1`;
 
 // Conectar ao MongoDB e iniciar o servidor Express
--- model/person.js
+++ model/person.js
@@ -1,1 +1,10 @@
 const mongoose=require('mongoose')
+
+//metodos para salvar e ler
+const Person=mongoose.model('Person',{
+    name:String,
+    salary:Number,
+    aproved:Boolean,
+})
+
+module.exports=Person
